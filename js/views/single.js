7111994d1109e538b226088e180178aead476508
Chris Dolphin <dolphin@likethemammal.com>
2014-02-09 00:01:55 -0500

    Made loading faster by removing images.

---
+++ js/views/single.js
@@ -0,0 +1,16 @@
+define(function(){
+	var SS = sock_singles.core;
+
+	var Grid = React.createClass({
+
+		render: function(){
+            var page;
+            
+            this.props.color
+
+          return page;
+        }
+	});
+
+	return Grid;
+});
