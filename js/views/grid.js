17a50e50a527b048b96d361bd7c16acd3dd5aa6e
Chris Dolphin <dolphin@likethemammal.com>
2014-02-09 00:05:24 -0500

    Merge conflict shit. Can't use vim cus of stupid lack of storage.

--- js/views/grid.js
+++ js/views/grid.js
@@ -16,7 +16,6 @@ define(["views/filter"],function(Filter){
 		render: function(){
           var grid,
               gridItems = [],
-<<<<<<< HEAD
               styles,
               caption,
               features,
@@ -28,29 +27,14 @@ define(["views/filter"],function(Filter){
 
       for (var i = 0; i < this.state.socks.length; i++) {
         styles = {
-          'background-image': ['url(', this.state.socks[i]['image-path'], ')'].join('')
+// 					'background-image': ['url(', this.state.socks[i]['image-path'], ')'].join('')
+					'background-image': ['url(/../imgs/sock', Math.ceil(Math.random()*3), 'clear.jpg)'].join('')
         }
 
         colorDiv = React.DOM.div({className: 'color-box', style: {background: this.state.socks[i].color}})
         colorFeature = React.DOM.div({ className: 'color feature'}, colorDiv);
         lengthFeature = React.DOM.div({ className: 'length feature' }, this.state.socks[i].length + '"');
         sizeFeature = React.DOM.div({ className: 'size feature' }, this.state.socks[i].size);
-=======
-			  styles,
-			  caption,
-			  features,
-			  item,
-			  itemWrapper,
-			  wrapperClasses;
-			  
-			  console.log(this.state);
-
-          for (var i = 0; i < this.state.socks.length; i++) {
-			  	styles = {
-// 					'background-image': ['url(', this.state.socks[i]['image-path'], ')'].join('')
-					'background-image': ['url(/../imgs/sock', Math.ceil(Math.random()*3), 'clear.jpg)'].join('')
-				}
->>>>>>> Made loading faster by removing images.
 
         features = React.DOM.div({ className: 'features' }, [colorFeature, lengthFeature, sizeFeature]);
         caption = React.DOM.div({ className: 'caption' }, this.state.socks[i]['title']);
